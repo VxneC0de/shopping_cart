417de7d0191e87a13a8376eb5e35522d055c9339
VxneC0de <vanerubio2020@gmail.com>
2024-08-07 16:52:45 -0400

    feat: strat register

--- connection.php
+++ connection.php
@@ -8,11 +8,13 @@ $db_password = '123456'; // contraseña del usuario
 // Crear la conexión
 $conn = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_password");
 
-// Comprobar si la conexión fue exitosa
+/* Comprobar si la conexión fue exitosa
 if (!$conn) {
     echo "Error al conectar a la base de datos: " . pg_last_error();
     exit;
 } else {
     echo "Conectado a la base de datos con éxito!";
 }
+*/
+
 ?>
